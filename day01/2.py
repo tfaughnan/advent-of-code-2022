5fab0017fe857043d4b5813652466faac5e06019
Thomas Faughnan <thomas@faughnan.net>
2022-12-01 00:41:50 -0500

    day01-2: shorter sort line (i swear im not golfing this year!)

--- day01/2.py
+++ day01/2.py
@@ -4,7 +4,7 @@ import sys
 
 def main():
     elves = [map(int, elf.split('\n')) for elf in sys.stdin.read()[:-1].split('\n\n')]
-    print(sum(sorted(map(sum, elves), reverse=True)[:3]))
+    print(sum(sorted(map(sum, elves))[-3:]))
 
 if __name__ == '__main__':
     main()
