250a772b48e52f5462ab1ea8378a493fa8549da3
Thomas Faughnan <thomas@faughnan.net>
2022-12-01 15:58:59 -0500

    day01: s/lists/generators

--- day01/1.py
+++ day01/1.py
@@ -3,7 +3,7 @@
 import sys
 
 def main():
-    elves = [map(int, elf.split('\n')) for elf in sys.stdin.read()[:-1].split('\n\n')]
+    elves = (map(int, elf.split('\n')) for elf in sys.stdin.read()[:-1].split('\n\n'))
     print(max(map(sum, elves)))
 
 if __name__ == '__main__':
--- day01/2.py
+++ day01/2.py
@@ -3,7 +3,7 @@
 import sys
 
 def main():
-    elves = [map(int, elf.split('\n')) for elf in sys.stdin.read()[:-1].split('\n\n')]
+    elves = (map(int, elf.split('\n')) for elf in sys.stdin.read()[:-1].split('\n\n'))
     print(sum(sorted(map(sum, elves))[-3:]))
 
 if __name__ == '__main__':
