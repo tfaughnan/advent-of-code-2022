e23a7318231053902c407a512692941973f19098
Thomas Faughnan <tom@tjf.sh>
2022-12-02 01:33:07 -0500

    day02: add solutions

---
+++ day02/1.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env pypy3
+
+import sys
+
+def main():
+    score = 0
+    for line in sys.stdin:
+        rnd = line.strip().split()
+        opp = ord(rnd[0]) - ord('A')    # ascii
+        me = ord(rnd[1]) - ord('X')     # haxx!
+        score += me + 1 + ((me - opp + 1) % 3) * 3
+
+    print(score)
+
+if __name__ == '__main__':
+    main()
