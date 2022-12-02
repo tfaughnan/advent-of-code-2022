586cbd63959def418754265d49f58e5f9e7013f5
Thomas Faughnan <tom@tjf.sh>
2022-12-02 03:19:27 -0500

    day02: remove redundant strip()

--- day02/2.py
+++ day02/2.py
@@ -5,7 +5,7 @@ import sys
 def main():
     score = 0
     for line in sys.stdin:
-        rnd = line.strip().split()
+        rnd = line.split()
         opp = ord(rnd[0]) - ord('A')                    # ascii
         me = (ord(rnd[1]) - ord('X') - 1 + opp) % 3     # haxx!
         score += me + 1 + ((me - opp + 1) % 3) * 3
