42cfa49c95e02e8389559614a3d1c8752b386bb0
Thomas Faughnan <thomas@faughnan.net>
2022-12-04 01:07:03 -0500

    day04: clean up solutions

--- day04/1.py
+++ day04/1.py
@@ -3,10 +3,10 @@
 import sys
 
 def main():
-    pairs = ([*map(lambda x: set(range(x[0], x[1] + 1)), x)] for x in
-             (map(lambda y: [*map(int, y.split('-'))], line.strip().split(','))
+    pairs = ([set(range(a, b + 1)) for a, b in c] for c in
+             (([*map(int, d.split('-'))] for d in line.strip().split(','))
               for line in sys.stdin))
-    overlaps = map(lambda x: x[0].issubset(x[1]) or x[1].issubset(x[0]), pairs)
+    overlaps = (a.issubset(b) or b.issubset(a) for a, b in pairs)
     print(sum(overlaps))
 
 if __name__ == '__main__':
