c294b16cbb5115f8913e7890b47739e8daa25c56
Thomas Faughnan <thomas@faughnan.net>
2022-12-04 23:27:08 -0500

    day04-1: wordsmithing

--- day04/1.py
+++ day04/1.py
@@ -6,8 +6,8 @@ def main():
     pairs = ([set(range(a, b + 1)) for a, b in c] for c in
              (([*map(int, d.split('-'))] for d in line.strip().split(','))
               for line in sys.stdin))
-    overlaps = (a.issubset(b) or b.issubset(a) for a, b in pairs)
-    print(sum(overlaps))
+    subsets = (a.issubset(b) or b.issubset(a) for a, b in pairs)
+    print(sum(subsets))
 
 if __name__ == '__main__':
     main()
