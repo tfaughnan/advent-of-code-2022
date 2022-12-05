3a05d50f0bc1b9cd3ccbbf2fb2436aff99a9da79
Thomas Faughnan <thomas@faughnan.net>
2022-12-05 00:53:38 -0500

    day05: add solutions

---
+++ day05/2.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env pypy3
+
+from collections import defaultdict
+import re
+import string
+import sys
+
+def build_stacks(lines):
+    stacks = defaultdict(list)
+    for line in lines.split('\n')[:-1]:
+        for col, char in enumerate(line):
+            if char in string.ascii_uppercase:
+                stacks[(col - 1) // 4].insert(0, char)
+
+    return stacks
+
+def move_crates(moves, stacks):
+    for move in moves.split('\n')[:-1]:
+        m = re.match(r'^move ([0-9]+) from ([0-9]+) to ([0-9]+)$', move.strip())
+        amount, stack_from, stack_to = map(int, m.groups())
+        crates = [stacks[stack_from - 1].pop() for _ in range(amount)][::-1]
+        stacks[stack_to - 1] += crates
+
+def main():
+    stack_lines, move_lines = sys.stdin.read().split('\n\n')
+    stacks = build_stacks(stack_lines)
+    move_crates(move_lines, stacks)
+    msg = ''.join(stacks[i][-1] for i in sorted(stacks.keys()))
+    print(msg)
+
+if __name__ == '__main__':
+    main()
