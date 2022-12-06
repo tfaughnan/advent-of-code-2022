e5eed358c3f987b056299ae91a955e6780694df8
Thomas Faughnan <thomas@faughnan.net>
2022-12-06 00:12:48 -0500

    day06: add solutions

---
+++ day06/1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env pypy3
+
+import sys
+
+WINSIZ = 4
+
+def main():
+    data = sys.stdin.read().strip()
+    for i in range(len(data) - WINSIZ):
+        if len(set(data[i:i + WINSIZ])) == WINSIZ:
+            print(i + WINSIZ)
+            break
+
+if __name__ == '__main__':
+    main()
